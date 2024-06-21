c79543db526c97d9b68267fd898977fb5ede6547
Full-Adder <2524614923@qq.com>
2024-06-21 21:14:46 +0800

    update metric

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import platform
 import random
 import numpy as np
 
+from sklearn.metrics import auc
 import torch
 import torchvision
 import torchaudio
 
 from dataset import get_dataloader
+from metrics.metrics import *
 
 torch.backends.cudnn.enabled = True
 torch.backends.cudnn.benchmark = True
@@ -26,12 +28,22 @@ saliency_path = r"/home/wjq/workplace/data/saliency_data/"
 
 if __name__ == "__main__":
     task_list = ['class_increase', 'domain_increase']
+    #  target['salmap'] = torch.ones(8, 1, 112, 112)
     print(f"use {platform.system()}, torch {torch.__version__}, torchvision {torchvision.__version__}, torchaudio {torchaudio.__version__}")
     for task in task_list:
         data_loader = get_dataloader(root=saliency_path, mode='train', task=task)
         print(f"{task= }, {len(data_loader)= }")
         for i,(data, target, vail) in enumerate(data_loader):
             print(i, data['rgb'].shape, data['audio'].shape, target['salmap'].shape, target['binmap'].shape)
+            cc = CC( target['salmap'], target['salmap'])
+            nss = NSS( target['salmap'], target['binmap'])
+            sim = SIM( target['salmap'], target['salmap'])
+            auc_judd = AUC_Judd( target['salmap'], target['binmap'])
+            auc_borji = AUC_Borji( target['salmap'], target['binmap'])
+            auc_shuffled = AUC_shuffled( target['salmap'], target['binmap'],target['binmap'])
+            print(f"{cc= }, {nss= }, {sim= }, {auc_judd= }, {auc_borji= }, {auc_shuffled= }")
             break
+    
+    
 
 
